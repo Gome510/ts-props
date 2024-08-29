5389e4a49d8342353f79b8e8313c847df7ac984f
Pablo Gomez-Echegaray <pgomezec@ucsc.edu>
2024-08-29 11:48:16 -0700

    consolidating generators

---
+++ src/generators/index.js
@@ -0,0 +1,76 @@
+const defaultArgTypes = {
+"number": `{
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100,
+      }
+    }`
+}
+
+const noArgType = ["string", "boolean", "object"]
+
+const defaultArgsValue = {
+  string: "'Sample Text'",
+  number: 0,
+  boolean: true,
+  array: "[]"
+};
+
+export function importStatements(filePath, fileName){  
+  return `import { ${fileName} as ${fileName}Component } from "${filePath}";\n`
+}
+
+export function defaultExport(name, args, argTypes){
+  return (
+    `
+export default {
+  title: '${name}',
+  component: ${name}Component,
+  argTypes: {
+${argTypes}
+  },
+  args: {
+${args}
+  },
+};
+`
+  )
+}
+
+export function argTypes(types){
+  const props = types.properties
+  let controlsArray = []
+  
+  Object.keys(props).forEach((key)=>{
+    const control = defaultArgTypes[props[key].type]
+    if(control && !noArgType.includes(props[key].type)){
+      controlsArray.push(`    ${key}: ${control}`)
+    }
+  })
+  
+  return controlsArray.join(',\n')
+}
+
+export function args(types) {
+  const props = types.properties
+  //const required = types.required
+  let argsArray = [];
+
+  Object.keys(props).forEach((key)=>{
+    //const isRequired = required.contains(key)
+    const argsText = defaultArgsValue[props[key].type]
+    argsArray.push(`    ${key}: ${argsText},`)
+  })
+
+  return argsArray.join('\n');
+}
+
+export function returnComponent(fileName){
+  return (
+`
+export const ${fileName} = (args: any) => {
+  return <${fileName}Component {...args} />;
+};`
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,41 +1,57 @@
-//@ts-ignore
 import { createGenerator } from "ts-json-schema-generator";
 import { writeFileSync, readdirSync, statSync } from "fs";
 import path from "path";
-import { args } from "./generators/args.js";
-import { argTypes } from "./generators/argTypes.js";
-import { Default } from "./generators/default.js";
 
-const repoRoot = process.cwd();
+import { args, argTypes, defaultExport, importStatements, returnComponent } from "./generators/index.js";
 
-const config = {
-  path: path.join(repoRoot, "src", ".tsx"),
-  tsconfig: path.join(repoRoot, "tsconfig.json"),
-  type: "Props",
-};
-const schema = createGenerator(config).createSchema(config.type);
-const schemaString = JSON.stringify(schema);
 
-function listFiles(directoryPath){
-  const files = readdirSync(directoryPath)
 
-  files.forEach(file => {
-    const filePath = path.join(directoryPath, file);
-    const fileStat = statSync(filePath);
+  const cwd = process.cwd();
+  const componentsDir = path.join(cwd,"components")
+  const storiesDir = path.join(cwd,"stories")
 
-    if(fileStat.isDirectory()){
-      listFiles(filePath)
-    }else if(filePath.includes(".tsx") && !filePath.includes("index")){
-      //make storybook file
-      const fileName = file.split(".")[0];
-      console.log(fileName)
-      const types = schema.definitions?.Props;
-      
-      console.log(filePath)
-      writeFileSync("output.txt",Default(fileName, args(types), argTypes(types)))
-    }
-  });
-  return;
-}
+  const config = {
+    path: path.join(componentsDir, "*"),
+    tsconfig: path.join(cwd, "tsconfig.json"),
+    type: "*",
+    expose: "all",
+    skipTypeCheck: true,
+  };
+  const schema = createGenerator(config).createSchema(config.type);
 
-listFiles(repoRoot)
+  function listFiles(directoryPath){
+    const files = readdirSync(directoryPath)
+
+    files.forEach(file => {
+      const filePath = path.join(directoryPath, file);
+      const fileStat = statSync(filePath);
+
+      if(fileStat.isDirectory()){
+        listFiles(filePath)
+      }else if(filePath.includes(".tsx") && !filePath.includes("index")){
+        let storybookText = ''
+
+        const fileName = file.split(".")[0];
+        const storybookFilePath = path.join(storiesDir,`${fileName}.stories.tsx`)
+        const relativePath = path.relative(storiesDir, filePath)
+        const types = schema.definitions?.Props;
+        
+        storybookText += importStatements(relativePath, fileName)
+        storybookText += defaultExport(fileName, args(types), argTypes(types))
+        storybookText += returnComponent(fileName)
+
+        writeFileSync(storybookFilePath, storybookText)
+      }
+    });
+    return;
+  }
+
+  listFiles(componentsDir)
+
+/* }else{
+  console.log(
+    `Usage Error: missing required arguments. 
+Expected Usage: npm run generateStories --components <components-dir-path> --stories <stories-dir-path>
+Note: paths need to be relative.
+`);
+} */
